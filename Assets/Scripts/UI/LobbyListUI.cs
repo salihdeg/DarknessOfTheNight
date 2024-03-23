43abccd24bc55980c70bc6895a17fc6d4c78c5db
Ecem <tugbaecemy@gmail.com>
2024-03-23 16:46:13 +0300

    Update LobbyListUI.cs

--- Assets/Scripts/UI/LobbyListUI.cs
+++ Assets/Scripts/UI/LobbyListUI.cs
@@ -14,6 +14,7 @@ public class LobbyListUI : MonoBehaviour
     [SerializeField] private TMP_InputField code;
     private void Awake()
     {
+        Instance = this;
         backButton.onClick.AddListener(() =>
         {
             Hide();
