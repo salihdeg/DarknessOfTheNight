36ca292a863f198a87a23c6269937f19a63a1996
Ecem <tugbaecemy@gmail.com>
2024-03-23 16:30:55 +0300

    Update CreateLobbyUI.cs

--- Assets/Scripts/UI/CreateLobbyUI.cs
+++ Assets/Scripts/UI/CreateLobbyUI.cs
@@ -1,14 +1,18 @@
 using Network;
 using TMPro;
+using UI;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CreateLobbyUI : MonoBehaviour
 {
+    public static CreateLobbyUI Instance { get; private set; }
     public TMP_InputField lobbyName;
-    public Button createPrivate, createPublic;
+    public Button createPrivate, createPublic,backButton;
+   
     private void Awake()
     {
+        Instance = this;
         createPrivate.onClick.AddListener(() =>
         {
             GameLobby.Instance.CreateLobby(lobbyName.text, true);
@@ -17,6 +21,23 @@ public class CreateLobbyUI : MonoBehaviour
         {
             GameLobby.Instance.CreateLobby(lobbyName.text, false);
         });
+        backButton.onClick.AddListener(() =>
+        {
+            Hide();
+            MainMenuUI.Instance.Show();
+        });
+    }
+    private void Start()
+    {
+        Hide();
+    }
+    public void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    public void Hide()
+    {
+        gameObject.SetActive(false);
     }
 
 }//class
